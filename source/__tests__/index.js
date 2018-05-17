f3c5fe9a86e7cdfc17e3949a6aeb256acd0113a2
Oleg Galaburda <burdiuz@gmail.com>
2018-05-17 12:09:16 +0300

    update linter setup, readme

--- source/__tests__/index.js
+++ source/__tests__/index.js
@@ -1,6 +1,8 @@
 import SomeString from '../index';
 
 describe('index', () => {
+  beforeEach(() => {});
+
   it('should export string as default', () => {
     expect(typeof SomeString).toEqual('string');
   });

b6ef2a8a84b1baa0c725889315846bb6c530efe4
Oleg Galaburda <burdiuz@gmail.com>
2018-05-17 09:55:21 +0300

    update dev dependencies

--- dist/lib.js
+++ dist/lib.js
@@ -1,31 +1,31 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global.lib = {})));
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (factory((global.lib = {})));
 }(this, (function (exports) { 'use strict';
 
-class MyClass {
-  constructor() {
-    this.value = 'void';
+  class MyClass {
+    constructor() {
+      this.value = 'void';
 
-    this.myMethod = arg => !!arg;
+      this.myMethod = arg => !!arg;
 
-    this.mySpreadMethod = (...args) => args;
+      this.mySpreadMethod = (...args) => args;
+    }
+
   }
 
-}
+  const obj = {};
+  const myFunc = () => {};
 
-const obj = {};
-const myFunc = () => {};
+  var index = 'Oh My!';
 
-var index = 'Oh My!';
+  exports.obj = obj;
+  exports.myFunc = myFunc;
+  exports.MyClass = MyClass;
+  exports.default = index;
 
-exports.obj = obj;
-exports.myFunc = myFunc;
-exports.MyClass = MyClass;
-exports['default'] = index;
-
-Object.defineProperty(exports, '__esModule', { value: true });
+  Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
 //# sourceMappingURL=lib.js.map
---
+++ install_dev.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+npm install \
+babel-core \
+babel-eslint \
+babel-loader \
+babel-plugin-external-helpers \
+babel-plugin-istanbul \
+babel-plugin-transform-class-properties \
+babel-plugin-transform-es2015-modules-commonjs \
+babel-plugin-transform-flow-strip-types \
+babel-plugin-transform-object-rest-spread \
+babel-preset-jest eslint eslint-config-airbnb \
+eslint-plugin-import eslint-plugin-jsx-a11y \
+eslint-plugin-react \
+flow-bin husky jest \
+lint-staged prettier \
+rollup-plugin-babel \
+rollup-plugin-commonjs \
+rollup-plugin-json \
+rollup-plugin-node-resolve \
+rollup-plugin-serve \
+rollup-plugin-uglify \
+uglify-es \
+--save-dev
